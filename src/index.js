c66b5c9e42a599346590130d486dc4031f6df749
KESHAV GUPTA <iamkeshavgupta27@gmail.com>
2022-12-18 11:40:13 +0530

    Added unique constraint and started cityrepository

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 // require('dotenv').config()
 // console.log(process.env);
 const { PORT } = require('./config/serverConfig');
+const CityRepository = require('./repository/city-repository');
 
 const setupAndStartServer = async (PORT) => {
     const app = express();
     // const PORT = 3000;
     app.listen(PORT, () => {
         console.log(`Server started at ${PORT}`);
+        let cityRepo = new CityRepository()
+        // cityRepo.createCity({ name: "Samastipur" })
     })
 };
 
